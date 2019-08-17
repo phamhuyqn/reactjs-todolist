075466af3d3eb3a4b89a5c46e07e0d645395be96
Huy Pham <phamhuyqn92@gmail.com>
2019-08-17 17:38:14 +0700

    Updates

---
+++ precache-manifest.994507f260acefd3f3558542aa1f11b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d7b0ec276a9399ca85acb668209d16",
+    "url": "/reactjs-todolist/index.html"
+  },
+  {
+    "revision": "5c5fbf46294a9e833978",
+    "url": "/reactjs-todolist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2fc02a975165f1af5618",
+    "url": "/reactjs-todolist/static/js/2.90420523.chunk.js"
+  },
+  {
+    "revision": "5c5fbf46294a9e833978",
+    "url": "/reactjs-todolist/static/js/main.1184d29a.chunk.js"
+  },
+  {
+    "revision": "c542bc0b69681b744a9c",
+    "url": "/reactjs-todolist/static/js/runtime~main.c243ac88.js"
+  }
+]);
