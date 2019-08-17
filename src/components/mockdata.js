7a7d917cf2771d3d0c24b8a39d14d813c919da18
Huy Pham <phamhuyqn92@gmail.com>
2019-08-17 18:15:03 +0700

    a

--- src/components/mockdata.js
+++ src/components/mockdata.js
@@ -2,7 +2,7 @@ var uniqid = require('uniqid');
 
 var listItems=
         [
-          { id: uniqid(), name: "Task 1", priority: 0 },
+          { id: uniqid(), name: "Task 12", priority: 0 },
           { id: uniqid(), name: "Task 2", priority: 1 },
           { id: uniqid(), name: "Task 3", priority: 2 },
           { id: uniqid(), name: "Task 4", priority: 1 },
